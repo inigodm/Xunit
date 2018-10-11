37c811c8e858a811612a663865e0f3e71695f0e2
inigodm <inigo.delgado@gmail.com>
2018-10-11 20:27:54 +0200

    Red: adding a report

--- testXUnit.py
+++ testXUnit.py
@@ -2,8 +2,14 @@ from XUnit import WasRun, TestCase
 
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
-       self.test = WasRun("testMethod")
-       self.test.run()
-       assert(self.test.log == "Setup testMethod tearDown")
+        self.test = WasRun("testMethod")
+        self.test.run()
+        assert(self.test.log == "Setup testMethod tearDown ")
+
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert (result.summary == "1 run, 0 failed")
 
 TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
